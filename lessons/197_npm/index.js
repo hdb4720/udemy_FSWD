d5a8ca56a03765fd583cebcca8c07df4f624f9b0
Harold D Breeden <hdb4720@gmail.com>
2024-03-07 17:05:06 -0600

    Publish after Node.js

---
+++ lessons/197_npm/index.js
@@ -0,0 +1,9 @@
+// var generateName = require('sillyname');
+import generateName from "sillyname";
+var sillyName = generateName();
+console.log(`My name is ${sillyName}`);
+
+import superheroes from "superheroes";
+var superheroName = superheroes.random();
+console.log(`My Super Hero name is ${superheroName}`);
+
